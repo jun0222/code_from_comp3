6c4ac183049d4c9314ecb73ff8d6db531a65024d
jun0222 <nextseason6@gmail.com>
2021-08-22 15:52:03 +0900

    add header & hamburger menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('pug', function(){
     gulp.src('src/*.pug')
@@ -14,4 +15,11 @@ gulp.task("sass", function(){
     gulp.src("scss/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("./css"))
+});
+
+//インラインで書き出し
+gulp.task( 'inlinesource', function() { //タスク「inlinesource」の内容を書こう
+    return gulp.src( 'index.html' ) //どのファイルに対して行うかを指定
+    .pipe( inlinesource() ) //インラインで読みこんで書き出し
+    .pipe( gulp.dest( '.' ) ); //「作業フォルダ/out」フォルダ内にファイルを作成
 });
